c942de29514b251709e260143f1f50bba744a93a
Seathorne <scottketclarke@gmail.com>
2025-06-13 19:40:02 -0400

    Add basic file-reading capability
    Added ability to open and close a file and read a few lines at a time.

---
+++ LineReader.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace LogViewer
+{
+    internal class LineReader : IDisposable
+    {
+        private StreamReader streamReader;
+
+        public bool Disposed { get; private set; }
+
+        public bool FileOpened { get; private set; }
+
+        public void OpenFile(string filePath)
+        {
+            if (Disposed)
+            {
+                throw new InvalidOperationException("This resource has already been disposed.");
+            }
+            else if (FileOpened)
+            {
+                throw new InvalidOperationException($"A file is already open. Use {nameof(CloseFile)} to close the file before opening another.");
+            }
+
+            streamReader = new StreamReader(filePath);
+            FileOpened = true;
+        }
+
+        public void CloseFile()
+        {
+            if (Disposed)
+            {
+                throw new InvalidOperationException("This resource has already been disposed.");
+            }
+            else if (!FileOpened)
+            {
+                throw new InvalidOperationException($"No file is currently open. Use {nameof(OpenFile)} to load a file.");
+            }
+
+            streamReader.Close();
+            FileOpened = false;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed)
+            {
+                throw new InvalidOperationException("This resource has already been disposed.");
+            }
+
+            streamReader.Dispose();
+            Disposed = true;
+        }
+
+        public IEnumerable<string> ReadLines(int numLines)
+        {
+            if (Disposed)
+            {
+                throw new InvalidOperationException("This resource has already been disposed.");
+            }
+            else if (!FileOpened)
+            {
+                throw new InvalidOperationException($"No file is currently open. Use {nameof(OpenFile)} to load a file before reading lines.");
+            }
+
+            int count = 0;
+            string line;
+            while ((line = streamReader.ReadLine()) != null && count < numLines)
+            {
+                count++;
+                yield return line;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LogViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly LineReader lineReader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lineReader = new LineReader();
+        }
+
+        private void OpenFile(string filename)
+        {
+            if (lineReader.FileOpened)
+            {
+                lineReader.CloseFile();
+            }
+
+            lineReader.OpenFile(filename);
+        }
+
+        private void CloseFile()
+        {
+            if (!lineReader.FileOpened) return;
+
+            lineReader.CloseFile();
+            LogTextBox.Clear();
+        }
+
+        private void ReadLines()
+        {
+            try
+            {
+                var lines = lineReader.ReadLines(10);
+                LogTextBox.Text = string.Join(Environment.NewLine, lines);
+            }
+            catch (InvalidOperationException e)
+            {
+                MessageBox.Show("Try opening a file first.", "Error reading lines", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ClickOnOpenFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|Log files (*.log)|*.log|All files (*.*)|*.*",
+                Title = "Open Log File"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filename = openFileDialog.FileName;
+                OpenFile(filename);
+            }
+        }
+
+        private void ClickOnCloseFile(object sender, RoutedEventArgs e)
+        {
+            CloseFile();
+        }
+
+        private void ClickOnCut(object sender, RoutedEventArgs e)
+        {
+            ApplicationCommands.Cut.Execute(null, Keyboard.FocusedElement);
+        }
+
+        private void ClickOnCopy(object sender, RoutedEventArgs e)
+        {
+            ApplicationCommands.Copy.Execute(null, Keyboard.FocusedElement);
+        }
+
+        private void ClickOnPaste(object sender, RoutedEventArgs e)
+        {
+            ApplicationCommands.Paste.Execute(null, Keyboard.FocusedElement);
+        }
+
+        private void ClickOnDelete(object sender, RoutedEventArgs e)
+        {
+            ApplicationCommands.Delete.Execute(null, Keyboard.FocusedElement);
+        }
+
+        private void ClickOnSelectAll(object sender, RoutedEventArgs e)
+        {
+            ApplicationCommands.SelectAll.Execute(null, Keyboard.FocusedElement);
+        }
+
+        private void KeyDownOnInputText(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ReadLines();
+            }
+        }
+
+        private void ClickOnInputEnterButton(object sender, RoutedEventArgs e)
+        {
+            ReadLines();
+        }
+    }
+}
